81e4f6baacfc3991717dac811c37909e5541c45e
Thomas Choi <thomastaechoi@gmail.com>
2016-03-09 12:16:59 -0800

    updating

--- back-end/controllers/flightController.js
+++ back-end/controllers/flightController.js
@@ -10,7 +10,7 @@ module.exports = {
       console.log('from back-end')
 
 
-      request("https://api.sandbox.amadeus.com/v1.2/flights/low-fare-search?apikey=Fyi0cubvIaMHSdV3EbAKLDFx4groZ32e&origin="+req.body.origin+"&destination="+req.body.destination+"&departure_date=2016-02-21&return_date=2016-02-27&adults="+req.body.adult+"&children="+req.body.child+"&infants="+req.body.infant+"&number_of_results=10",
+      request("https://api.sandbox.amadeus.com/v1.2/flights/low-fare-search?apikey=ND2UxPwIEwnbdVIIfwEZFBs79cD1x4ah&origin="+req.body.origin+"&destination="+req.body.destination+"&departure_date=2016-03-04&return_date=2016-03-06&adults="+req.body.adult+"&children="+req.body.child+"&infants="+req.body.infant+"&number_of_results=10",
 
         function (err, response, body) {
           if (body) {
